82441b37b52b652f45385e525fcc666bbe103a06
pratik1008 <nandanikarpratik@gmail.com>
2017-08-06 20:32:43 +0530

    Add files via upload

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,24 @@
-console.log("yohoooo");
+import React from 'react';
+import { render } from 'react-dom';
+import axios from 'axios';
+
+export default class Index extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+    render() {
+        axios.get('http://localhost:18080/messenger/webapi/hello', '').then(function(response) {
+            console.log(response);
+        }).catch(function (error){
+            console.log(error);
+        });
+        return (
+        <div>
+            <h1> Hey you </h1>
+        </div>
+        );
+    }
+
+}
+render(<Index/>, document.getElementById("index"));
